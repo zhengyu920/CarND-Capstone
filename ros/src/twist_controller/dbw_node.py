41579435f609f3acd942db8222aadd2028f19485
yu <zhengyu920@gmail.com>
2019-03-10 23:33:22 -0700

    fixed dbw_node

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -61,7 +61,7 @@ class DBWNode(object):
         self.throttle = self.steering = self.brake = 0
 
         # TODO: Create `Controller` object
-        self.controller = Controller()
+        self.controller = Controller(vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
 
